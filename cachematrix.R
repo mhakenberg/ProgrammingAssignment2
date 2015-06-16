aa8852dbd6b8267c833406604de962912a1d0985
mhakenberg <m.hakenberg@gmail.com>
2015-06-16 11:13:07 +0200

    with acknowledgements

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Aknowlegments
+## Both fuctions are based on examples posted on Coursera R Programming Assignment2       
+## webpage  - they were modified  and tested by me  
+
 ## My functions prevent from repeating time-costly operation of inverisng the same matrix many times - storing 
 ## previously calculated inverses in a matrix-cache object
 
 ## Write a short comment describing this function
 ## makeCacheMatrix creates a special list where input matrix values (from input parameter(matrix) x) and its inverse 
-## matrix (if previously computed by cacheSolve) are stored, ready to access by cacheSolve function
+## matrix (if previously computed by cacheSolve) are stored, ready for access by cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -25,7 +29,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 ## cacheSolve checks for inverse matrix stored in a list created by makeCacheMatrix for this specific matrix 
-## and if there is no inverse matrix stored (for input parameter x) then reads matrix stored in the list, 
+## and if there is no inverse matrix stored then reads matrix stored in the list, 
 ## computes the inverse and stores it in the list for future use
 
 cacheSolve <- function(x, ...) {
